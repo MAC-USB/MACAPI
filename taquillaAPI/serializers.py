e65f4bd6e4b199a6b1469a0b6b930f02ff7063ca
josedavidb <josedavidb6@gmail.com>
2018-08-20 22:39:15 -0400

    Agregado todos los Serializers y las View de los Serializers

--- taquillaAPI/serializers.py
+++ taquillaAPI/serializers.py
@@ -1,7 +1,109 @@
 from rest_framework import serializers
-from taquillaAPI.models import Articulo
+from taquillaAPI.models import *
+
+"""
+ Serializers para hacer funcionar el Rest framework
+"""
+
 
 class ArticuloSerializer(serializers.ModelSerializer):
+
+	"""
+	Consiste en el serializer del modelo Articulo.
+
+	Campos que se van a pasar: nombre, precio
+	"""
 	class Meta:
 		model = Articulo
 		fields = ('nombre','precio')
+
+class ClienteSerializer(serializers.ModelSerializer):
+	"""
+	Consiste en el serializer del modelo Cliente.
+
+	Campos que se van a pasar: nombre, precio
+	"""
+	class Meta:
+		model = Cliente
+		fields = ('cedula', 'nombre','apellido','telefono')
+
+
+class InteresSerializer(serializers.ModelSerializer):
+	"""
+	Consiste en el serializer del modelo Cliente.
+
+	Campos que se van a pasar: porcentaje, rango_dias
+	"""
+	class Meta:
+		model = Interes
+		fields = ('porcentaje', 'rango_dias')
+
+class PreparadorSerializer(serializers.ModelSerializer):
+	"""
+	Consiste en el serializer del modelo Preparador.
+
+	Campos que se van a pasar: cedula, iniciales, nombre, apellido, correo, cantidad_deuda, fecha_deuda
+	"""
+	class Meta:
+		model = Interes
+		fields = ('cedula', 'iniciales','nombre','apellido','correo','cantidad_deuda','fecha_deuda')
+
+class HistorialCuentaSerializer(serializers.ModelSerializer):
+	"""
+	Consiste en el serializer del modelo HistorialCuenta.
+
+	Campos que se van a pasar: fecha, cant_ideal_efectivo, cant_ideal_caja, cant_real_efectivo,cant_real_caja
+	"""
+	class Meta:
+		model = HistorialCuenta
+		fields = ('fecha','cant_ideal_efectivo','cant_ideal_caja','cant_real_efectivo','cant_real_caja')
+
+class PlataformaPago(serializers.ModelSerializer):
+	"""
+	Consiste en el serializer del modelo PlataformaPago
+
+	Campos que se van a pasar: fecha, cant_ideal_efectivo, cant_ideal_caja, cant_real_efectivo,cant_real_caja
+	"""
+	class Meta:
+		model = PlataformaPago
+		fields = ('nombre')
+
+class Transaccion(serializers.ModelSerializer):
+	"""
+	Consiste en el serializer del modelo Transaccion.
+
+	Campos que se van a pasar: fecha, monto, tipo
+	"""
+	class Meta:
+		model = PlataformaPago
+		fields = ('fecha','monto','tipo')
+
+class Venta(serializers.ModelSerializer):
+	"""
+	Consiste en el serializer del modelo Venta.
+
+	Campos que se van a pasar: id_transaccion, cantidad_producto, articulo, tipoPago, nro_confirmacion, plataforma_pago, cliente, preparador
+	"""
+	class Meta:
+		model = PlataformaPago
+		fields = ('id_transaccion','cantidad_producto','articulo','tipoPago','nro_confirmacion','plataforma_pago','cliente','preparador')
+
+class Deuda(serializers.ModelSerializer):
+	"""
+	Consiste en el serializer del modelo Deuda.
+
+	Campos que se van a pasar: id_transaccion, articulo, cantidad_producto, preparador
+	"""
+	class Meta:
+		model = PlataformaPago
+		fields = ('id_transaccion','articulo','cantidad_producto','preparador')
+
+class PagoDeuda(serializers.ModelSerializer):
+	"""
+	Consiste en el serializer del modelo PagoDeuda.
+
+	Campos que se van a pasar: id_transaccion, montoDeuda, tipoPago, nro_confirmacion, plataforma_pago, fecha_pago, preparador
+	"""
+	class Meta:
+		model = PlataformaPago
+		fields = ('id_transaccion','montoDeuda','tipoPago','nro_confirmacion','plataforma_pago','fecha_pago','preparador')
--- taquillaAPI/views.py
+++ taquillaAPI/views.py
@@ -1,10 +1,54 @@
 from django.shortcuts import render
-from taquillaAPI.models import Articulo
-from taquillaAPI.serializers import ArticuloSerializer
+from taquillaAPI.models import *
+from taquillaAPI.serializers import *
 from rest_framework import generics
 
 # Create your views here.
 
+"""
+
+Views del Restframework
+
+"""
+
 class ArticuloList(generics.ListCreateAPIView):
 	queryset = Articulo.objects.all()
 	serializer_class = ArticuloSerializer
+
+class ClienteList(generics.ListCreateAPIView):
+	queryset = Cliente.objects.all()
+	serializers_class = ClienteSerializer
+
+class InteresList(generics.ListCreateAPIView):
+	queryset = Interes.objects.all()
+	serializers_class = InteresSerializer	
+
+class PreparadorList(generics.ListCreateAPIView):
+	queryset = Preparador.objects.all()
+	serializers_class = PreparadorSerializer
+
+class HistorialCuentaList(generics.ListCreateAPIView):
+	queryset = HistorialCuenta.objects.all()
+	serializers_class = HistorialCuentaSerializer
+
+"""
+class PlataformaPagoList(generics.ListCreateAPIView):
+	queryset = PlataformaPago.objects.all()
+	serializers_class = PlataformaPagoSerializer
+
+class TransaccionList(generics.ListCreateAPIView):
+	queryset = Transaccion.objects.all()
+	serializers_class = TransaccionSerializer
+
+class VentaList(generics.ListCreateAPIView):
+	queryset = Venta.objects.all()
+	serializers_class = VentaSerializer
+
+class DeudaList(generics.ListCreateAPIView):
+	queryset = Deuda.objects.all()
+	serializers_class = DeudaSerializer
+
+class PagoDeudaList(generics.ListCreateAPIView):
+	queryset = PagoDeuda.objects.all()
+	serializers_class = PagoDeudaSerializer
+"""
