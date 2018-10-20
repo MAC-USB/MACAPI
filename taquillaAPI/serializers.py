07919bf3434c068522147ffa1027b765b0e11593
Jose Basanta <josedavidb6@gmail.com>
2018-10-20 16:44:21 -0400

    Added endpoints to edit, delete and retrieve for each model

--- taquillaAPI/serializers.py
+++ taquillaAPI/serializers.py
@@ -29,9 +29,10 @@ class ClienteSerializer(serializers.ModelSerializer):
 
 class ClienteUpdateSerializer(serializers.ModelSerializer):
 	"""
-	Consiste en el serializer del modelo Cliente.
+	Consiste en el serializer update del modelo Cliente.
 
 	Campos que se van a pasar: nombre, precio
+	Campos que solo se van a leer: cedula
 	"""
 	class Meta:
 		model = Cliente
@@ -40,7 +41,7 @@ class ClienteUpdateSerializer(serializers.ModelSerializer):
 
 class InteresSerializer(serializers.ModelSerializer):
 	"""
-	Consiste en el serializer del modelo Cliente.
+	Consiste en el serializer del modelo Interes.
 
 	Campos que se van a pasar: porcentaje, rango_dias
 	"""
@@ -58,6 +59,18 @@ class PreparadorSerializer(serializers.ModelSerializer):
 		model = Preparador
 		fields = ('cedula', 'iniciales','nombre','apellido','cantidad_deuda','fecha_deuda')
 
+class PreparadorUpdateSerializer(serializers.ModelSerializer):
+	"""
+	Consiste en el serializer update del modelo Preparador.
+
+	Campos que se van a pasar: cedula, iniciales, nombre, apellido, correo, cantidad_deuda, fecha_deuda
+	Campos que solo se van a leer: cedula
+	"""
+	class Meta:
+		model = Preparador
+		fields = ('cedula', 'iniciales','nombre','apellido','cantidad_deuda','fecha_deuda')
+		read_only_fields = ('cedula',)
+
 class HistorialCuentaSerializer(serializers.ModelSerializer):
 	"""
 	Consiste en el serializer del modelo HistorialCuenta.
@@ -67,6 +80,18 @@ class HistorialCuentaSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = HistorialCuenta
 		fields = ('fecha','cant_ideal_efectivo','cant_ideal_caja','cant_real_efectivo','cant_real_caja')
+
+class HistorialCuentaUpdateSerializer(serializers.ModelSerializer):
+	"""
+	Consiste en el serializer update del modelo Historial Cuenta.
+
+	Campos que se van a pasar: fecha, cant_ideal_efectivo, cant_ideal_caja, cant_real_efectivo,cant_real_caja
+	Campos que solo se van a leer: fecha
+	"""
+	class Meta:
+		model = Cliente
+		fields = ('fecha','cant_ideal_efectivo','cant_ideal_caja','cant_real_efectivo','cant_real_caja')
+		read_only_fields = ('fecha',)
 
 class PlataformaPagoSerializer(serializers.ModelSerializer):
 	"""
--- taquillaAPI/views.py
+++ taquillaAPI/views.py
@@ -16,6 +16,10 @@ class ArticuloList(generics.ListCreateAPIView):
 	queryset = Articulo.objects.all()
 	serializer_class = ArticuloSerializer
 
+class ArticuloRetrieve(generics.RetrieveAPIView):
+	queryset = Articulo.objects.all()
+	serializer_class = ArticuloSerializer
+
 class ArticuloUpdate(generics.UpdateAPIView):
 	queryset = Articulo.objects.all()
 	serializer_class = ArticuloSerializer
@@ -27,7 +31,10 @@ class ArticuloDestroy(generics.DestroyAPIView):
 class ClienteList(generics.ListCreateAPIView):
 	queryset = Cliente.objects.all()
 	serializer_class = ClienteSerializer
-	
+
+class ClienteRetrieve(generics.RetrieveAPIView):
+	queryset = Cliente.objects.all()
+	serializer_class = ClienteSerializer
 
 class ClienteUpdate(generics.UpdateAPIView):
 	queryset = Cliente.objects.all()
@@ -41,17 +48,74 @@ class ClienteDestroy(generics.DestroyAPIView):
 
 class InteresList(generics.ListCreateAPIView):
 	queryset = Interes.objects.all()
-	serializer_class = InteresSerializer	
+	serializer_class = InteresSerializer
+
+class InteresRetrieve(generics.RetrieveAPIView):
+	queryset = Interes.objects.all()
+	serializer_class = InteresSerializer
+
+class InteresUpdate(generics.UpdateAPIView):
+	queryset = Interes.objects.all()
+	serializer_class = InteresSerializer
+
+
+class InteresDestroy(generics.DestroyAPIView):
+	queryset = Interes.objects.all()
+	serializer_class = ClienteSerializer
+		
 
 class PreparadorList(generics.ListCreateAPIView):
 	queryset = Preparador.objects.all()
 	serializer_class = PreparadorSerializer
 
+class PreparadorRetrieve(generics.RetrieveAPIView):
+	queryset = Preparador.objects.all()
+	serializer_class = PreparadorSerializer
+	lookup_field = 'cedula'
+
+class PreparadorUpdate(generics.UpdateAPIView):
+	queryset = Preparador.objects.all()
+	serializer_class = PreparadorUpdateSerializer
+	lookup_field = 'cedula'
+
+class PreparadorDestroy(generics.DestroyAPIView):
+	queryset = Preparador.objects.all()
+	serializer_class = PreparadorSerializer
+	lookup_field = 'cedula'
+
 class HistorialCuentaList(generics.ListCreateAPIView):
 	queryset = HistorialCuenta.objects.all()
 	serializer_class = HistorialCuentaSerializer
 
+class HistorialCuentaRetrieve(generics.RetrieveAPIView):
+	queryset = HistorialCuenta.objects.all()
+	serializer_class = HistorialCuentaSerializer
+	lookup_field = 'fecha'
+
+class HistorialCuentaUpdate(generics.UpdateAPIView):
+	queryset = HistorialCuenta.objects.all()
+	serializer_class = HistorialCuentaUpdateSerializer
+	lookup_field = 'cedula'
+
+class HistorialCuentaDestroy(generics.DestroyAPIView):
+	queryset = HistorialCuenta.objects.all()
+	serializer_class = HistorialCuentaSerializer
+	lookup_field = 'cedula'
+
+
 class PlataformaPagoList(generics.ListCreateAPIView):
+	queryset = PlataformaPago.objects.all()
+	serializer_class = PlataformaPagoSerializer
+
+class PlataformaPagoRetrieve(generics.RetrieveAPIView):
+	queryset = PlataformaPago.objects.all()
+	serializer_class = PlataformaPagoSerializer
+
+class PlataformaPagoUpdate(generics.UpdateAPIView):
+	queryset = PlataformaPago.objects.all()
+	serializer_class = PlataformaPagoSerializer
+
+class PlataformaPagoDestroy(generics.DestroyAPIView):
 	queryset = PlataformaPago.objects.all()
 	serializer_class = PlataformaPagoSerializer
 
@@ -59,15 +123,65 @@ class TransaccionList(generics.ListCreateAPIView):
 	queryset = Transaccion.objects.all()
 	serializer_class = TransaccionSerializer
 
+class TransaccionRetrieve(generics.RetrieveAPIView):
+	queryset = Transaccion.objects.all()
+	serializer_class = TransaccionSerializer
+
+
+class TransaccionUpdate(generics.UpdateAPIView):
+	queryset = Transaccion.objects.all()
+	serializer_class = TransaccionSerializer
+
+
+class TransaccionDestroy(generics.DestroyAPIView):
+	queryset = Transaccion.objects.all()
+	serializer_class = TransaccionSerializer
+
 class VentaList(generics.ListCreateAPIView):
 	queryset = Venta.objects.all()
 	serializer_class = VentaSerializer
+
+class VentaRetrieve(generics.RetrieveAPIView):
+	queryset = Preparador.objects.all()
+	serializer_class = PreparadorSerializer
+
+class VentaUpdate(generics.UpdateAPIView):
+	queryset = Preparador.objects.all()
+	serializer_class = PreparadorUpdateSerializer
+
+class VentaDestroy(generics.DestroyAPIView):
+	queryset = Preparador.objects.all()
+	serializer_class = PreparadorSerializer
 
 class DeudaList(generics.ListCreateAPIView):
 	queryset = Deuda.objects.all()
 	serializer_class = DeudaSerializer
 
+class DeudaRetrieve(generics.RetrieveAPIView):
+	queryset = Preparador.objects.all()
+	serializer_class = PreparadorSerializer
+
+class DeudaUpdate(generics.UpdateAPIView):
+	queryset = Preparador.objects.all()
+	serializer_class = PreparadorUpdateSerializer
+
+class DeudaDestroy(generics.DestroyAPIView):
+	queryset = Preparador.objects.all()
+	serializer_class = PreparadorSerializer
+
 class PagoDeudaList(generics.ListCreateAPIView):
 	queryset = PagoDeuda.objects.all()
 	serializer_class = PagoDeudaSerializer
+
+class PagoDeudaRetrieve(generics.RetrieveAPIView):
+	queryset = Preparador.objects.all()
+	serializer_class = PreparadorSerializer
+
+class PagoDeudaUpdate(generics.UpdateAPIView):
+	queryset = Preparador.objects.all()
+	serializer_class = PreparadorUpdateSerializer
+
+class PagoDeudaDestroy(generics.DestroyAPIView):
+	queryset = Preparador.objects.all()
+	serializer_class = PreparadorSerializer
 
